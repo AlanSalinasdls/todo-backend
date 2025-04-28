c12d216a7b0031b5c48c1fe7f70facd037814a54
AlanSalinasdls <alansdlsdev@gmail.com>
2025-04-27 23:25:22 -0600

    chore: create users module

---
+++ src/users/entity/user.entity.ts
@@ -0,0 +1,29 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+import { Task } from '../../tasks/entity/task.entity';
+
+@ObjectType()
+@Entity()
+export class User {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Field()
+  @Column()
+  fullname: string;
+
+  @Field()
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Field()
+  @Column()
+  createdAt: Date;
+
+  @OneToMany(() => Task, (task) => task.user)
+  tasks: Task[];
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './entity/user.entity';
+import { CreateUserInput } from './dto/create-user.input';
+import * as bcrypt from 'bcryptjs';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+
+  async findOneByEmail(email: string): Promise<User | undefined> {
+    return this.usersRepository.findOne({ where: { email } });
+  }
+
+  async findOneById(id: string): Promise<User | undefined> {
+    return this.usersRepository.findOne({ where: { id } });
+  }
+
+  async create(createUserInput: CreateUserInput): Promise<User> {
+    const hashedPassword = await bcrypt.hash(createUserInput.password, 10);
+    const user = this.usersRepository.create({
+      ...createUserInput,
+      password: hashedPassword,
+      createdAt: new Date(),
+    });
+    return this.usersRepository.save(user);
+  }
+}
